d206a86212ddbfa82b370f77d6afe8c8505c5128
Shamayko <shamayko@gmail.com>
2022-04-05 10:18:44 +0300

    Initial commit

---
+++ src/main/java/ru/shamayko/spring/march/market/SpringMarchMarketApplication.java
@@ -0,0 +1,13 @@
+package ru.shamayko.spring.march.market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMarchMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMarchMarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/shamayko/spring/march/market/SpringMarchMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.shamayko.spring.march.market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMarchMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
